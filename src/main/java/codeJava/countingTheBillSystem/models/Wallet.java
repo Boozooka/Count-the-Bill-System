95180f85895d90dc4c288cc3f22ced5fa8348fa4
Boozooka <karhu00@list.ru>
2024-10-19 13:42:34 +0300

    Add Wallet entity and service

---
+++ src/main/java/codeJava/countingTheBillSystem/models/Wallet.java
@@ -0,0 +1,21 @@
+package codeJava.countingTheBillSystem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Wallet {
+
+    @Id
+    UUID uuid;
+
+    Long amount;
+}
