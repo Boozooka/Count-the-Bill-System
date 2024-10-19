1e756eeb4ab3348cc241ff745f3e316a650225f7
Boozooka <karhu00@list.ru>
2024-10-19 09:10:04 +0300

    Add Spring.run

--- src/main/java/codeJava/countingTheBillSystem/CountingTheBillSystemApplication.java
+++ src/main/java/codeJava/countingTheBillSystem/CountingTheBillSystemApplication.java
@@ -1,4 +1,11 @@
 package codeJava.countingTheBillSystem;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CountingTheBillSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CountingTheBillSystemApplication.class, args);
+    }
 }
