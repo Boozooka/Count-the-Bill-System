43606bf4291fded739167cb3bc8ad262e91e95c2
Boozooka <karhu00@list.ru>
2024-10-19 10:05:51 +0300

    Add BillController

---
+++ src/main/java/codeJava/countingTheBillSystem/controllers/BillController.java
@@ -0,0 +1,11 @@
+package codeJava.countingTheBillSystem.controllers;
+
+import codeJava.countingTheBillSystem.dto.requests.ChangeBillRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BillController {
+
+    
+}
